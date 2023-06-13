9934e68fceb95a72b9a805bab9a1804f58326560
Soumyadeep-Sinha <soumyadeep8402@gmail.com>
2023-06-13 23:00:18 +0530

    css updates

--- routes/search.js
+++ routes/search.js
@@ -10,10 +10,14 @@ router.post("/search", async (req, res) => {
         res.redirect("/login");
     }
     const username = req.body.searchName;
+    if(req.session.username === username){
+        res.redirect("/profile");
+    }
     // console.log(username);
     const search = await User.findOne({ UserName: username });
     if (!search) {
-        res.render("errorPage", { code: "404", message: "NOT FOUND" })
+        res.status(404);
+        res.render("errorPage", { code: "404", message: "NOT FOUND" });
     } else {
         const id = search._id;
 
@@ -44,8 +48,53 @@ router.post("/search", async (req, res) => {
             res.redirect("/login");
         }
     }
+});
 
-})
+router.get("/search/:something", async (req, res) => {
+    if (!req.session._id) {
+        res.redirect("/login");
+    }
+    const username = req.params.something;
+    console.log(username);
+    if(req.session.username === username){
+        res.redirect("/profile");
+    }
+    // console.log(username);
+    const search = await User.findOne({ UserName: username });
+    if (!search) {
+        res.status(404);
+        res.render("errorPage", { code: "404", message: "NOT FOUND" });
+    } else {
+        const id = search._id;
+
+        let year = new Date().getFullYear();
+        if (id) {
+            await Post.find({ Uploader: id })
+                .sort({ CreatedAt: -1 })
+                .then(posts => {
+                    posts.forEach(post => {
+                        post.ImageUrl = post.ImageUrl.replace(/\\/g, '/');
+                        post.ImageUrl = post.ImageUrl.replace(/public\//, '');
+                    });
+                    res.render("gen-profile",
+                        {
+                            username,
+                            year,
+                            posts,
+                            totalLikes: search.TotalLikes,
+                            totalAwards: search.TotalAwards,
+                            Bio: search.Bio
+                        });
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        } else {
+            console.log("NOT LOGGED IN!");
+            res.redirect("/login");
+        }
+    }
+});
 
 
 router.post("/searchPost", async (req, res) => {
